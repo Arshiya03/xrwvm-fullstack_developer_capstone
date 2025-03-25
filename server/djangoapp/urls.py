f74967309c8d6613d7ebac7f4d823accd515243a
Arshiya03 <aseemalatheef@gmail.com>
2025-03-25 04:11:45 -0400

    Fixed

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,8 +6,8 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='register', view=views.registration,name='register'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+    path(route='register', view=views.registration, name='register'),
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+
 # Create a `login_user` view to handle sign in request
 @csrf_exempt
 def login_user(request):
@@ -26,6 +27,7 @@ def login_user(request):
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
+
 
 # Create a `logout_request` view to handle sign out request
 @csrf_exempt
